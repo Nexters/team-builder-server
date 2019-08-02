06c9c1fd87cbd2ce2c7ecbe005e708b49b0f99ca
originman <origin_man@naver.com>
2019-08-02 19:46:08 +0900

    CORS 문제로 @CrossOrigin 부착

--- src/main/java/com/nexters/teambuilder/user/api/AuthController.java
+++ src/main/java/com/nexters/teambuilder/user/api/AuthController.java
@@ -3,10 +3,12 @@ package com.nexters.teambuilder.user.api;
 import com.nexters.teambuilder.user.api.dto.UserResponse;
 import com.nexters.teambuilder.user.domain.User;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/apis")
 public class AuthController {
--- src/main/java/com/nexters/teambuilder/user/api/UserController.java
+++ src/main/java/com/nexters/teambuilder/user/api/UserController.java
@@ -6,12 +6,14 @@ import com.nexters.teambuilder.user.api.dto.UserRequest;
 import com.nexters.teambuilder.user.api.dto.UserResponse;
 import com.nexters.teambuilder.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("users")
