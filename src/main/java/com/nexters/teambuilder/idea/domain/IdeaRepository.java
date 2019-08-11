af7fdb11b01c779d6a2082f91fcc8beb40ef1c72
sojeongw <redthink90@gmail.com>
2019-08-11 09:40:37 +0900

    fix/#4: refactor idea API

--- src/main/java/com/nexters/teambuilder/idea/domain/IdeaRepository.java
+++ src/main/java/com/nexters/teambuilder/idea/domain/IdeaRepository.java
@@ -3,7 +3,5 @@ package com.nexters.teambuilder.idea.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface IdeaRepository extends JpaRepository<Idea, Integer> {
-
 }
--- src/main/java/com/nexters/teambuilder/tag/domain/Tag.java
+++ src/main/java/com/nexters/teambuilder/tag/domain/Tag.java
@@ -25,6 +25,7 @@ public class Tag {
     private Integer tagId;
 
     private String name;
+
     @Enumerated(EnumType.STRING)
     private Type type;
 
