9ccbedb9b348ffd3a20c1201590057ac12a4e5de
namkiwon <origi_man@naver.com>
2019-08-17 15:29:30 +0900

    swagger 회원가입 필드 설명 추가

--- src/main/java/com/nexters/teambuilder/user/api/UserController.java
+++ src/main/java/com/nexters/teambuilder/user/api/UserController.java
@@ -5,6 +5,9 @@ import com.nexters.teambuilder.user.api.dto.SignInResponse;
 import com.nexters.teambuilder.user.api.dto.UserRequest;
 import com.nexters.teambuilder.user.api.dto.UserResponse;
 import com.nexters.teambuilder.user.service.UserService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +23,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
+    @ApiOperation(value = "회원가")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "originman", required = true, dataType = "string", paramType = "body"),
+            @ApiImplicitParam(name = "password", value = "1234", required = true, dataType = "string", paramType = "body"),
+            @ApiImplicitParam(name = "name", value = "namkiwon", required = true, dataType = "string", paramType = "body"),
+            @ApiImplicitParam(name = "nextersNumber", value = "1", required = true, dataType = "number", paramType = "body"),
+            @ApiImplicitParam(name = "role", value = "{ROLE_ADMIN or ROLE_USER}", required = true, dataType = "string", paramType = "body"),
+            @ApiImplicitParam(name = "position", value = "{DESIGNER or DEVELOPER}", required = true, dataType = "string", paramType = "body"),
+    })
     @PostMapping("sign-up")
     public BaseResponse<UserResponse> signUp(@RequestBody UserRequest request) {
         UserResponse user = userService.createUser(request);
