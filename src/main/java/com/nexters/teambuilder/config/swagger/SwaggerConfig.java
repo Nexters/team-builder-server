d81eb74f47439f9447a196613d8f097cedc9390c
namkiwon <origi_man@naver.com>
2019-08-18 01:02:31 +0900

    swagger set access token

--- src/main/java/com/nexters/teambuilder/config/swagger/SwaggerConfig.java
+++ src/main/java/com/nexters/teambuilder/config/swagger/SwaggerConfig.java
@@ -1,9 +1,15 @@
 package com.nexters.teambuilder.config.swagger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,7 +20,17 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
+
+        List<Parameter> global = new ArrayList<>();
+        global.add(new ParameterBuilder()
+                .name("Authorization")
+                .description("Access Token")
+                .parameterType("header")
+                .required(false)
+                .modelRef(new ModelRef("string")).build());
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(global)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
