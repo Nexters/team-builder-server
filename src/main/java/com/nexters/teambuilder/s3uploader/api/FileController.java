ff4950466dacbf4d0fe8ebeb05a3ab6b34a007d2
namkiwon <origi_man@naver.com>
2019-08-21 20:57:33 +0900

    file uploader

--- src/main/java/com/nexters/teambuilder/s3uploader/api/FileController.java
+++ src/main/java/com/nexters/teambuilder/s3uploader/api/FileController.java
@@ -3,6 +3,7 @@ package com.nexters.teambuilder.s3uploader.api;
 import java.util.List;
 
 import com.nexters.teambuilder.s3uploader.service.AmazonS3Service;
+import com.nexters.teambuilder.s3uploader.service.S3Uploader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,10 +19,19 @@ public class FileController {
     @Autowired
     private AmazonS3Service amazonS3Service;
 
+    @Autowired
+    private S3Uploader s3Uploader;
+
     @PostMapping(value = "/upload", params = {"targetPath!=", "filename!="})
     public List<String> uploadFile(@RequestParam List<MultipartFile> images,
                                    @RequestParam String targetPath,
                                    @RequestParam String filename) {
         return amazonS3Service.uploadImages(targetPath, filename, images);
+    }
+
+    @PostMapping(value = "/upload-file")
+    public String upload(@RequestParam MultipartFile file,
+                         @RequestParam String dirName) throws Exception {
+        return s3Uploader.upload(file, dirName);
     }
 }
