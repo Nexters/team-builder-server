4fea29cf5e81d5142924f6ce5a04cc60be41f7dc
sojeongw <redthink90@gmail.com>
2019-12-26 16:04:30 +0900

    issue/#176: [팀빌딩] 멤버 업데이트 시 hasTeam이 false로 설정되던 부분을 true로 변경

--- src/main/java/com/nexters/teambuilder/idea/domain/Idea.java
+++ src/main/java/com/nexters/teambuilder/idea/domain/Idea.java
@@ -116,7 +116,7 @@ public class Idea {
     }
 
     public void addMember(List<User> members) {
-        this.members.stream().forEach(member -> member.updateHasTeam(false));
+        this.members.stream().forEach(member -> member.updateHasTeam(true));
         this.author.updateHasTeam(true);
         this.members.clear();
         this.members.add(this.author);
